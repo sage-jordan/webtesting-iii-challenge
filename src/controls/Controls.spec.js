9de79f7a13f78b1f38a43a09ade43e78f24fc90a
sage-jordan <sagemjrodan@gmail.com>
2019-10-03 14:34:12 -0400

    added Dahboard and Controls tests, and @testing-

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -3,4 +3,41 @@
 // - buttons' text changes to reflect the state the door will be in if clicked
 // - the closed toggle button is disabled if the gate is locked
 // - the locked toggle button is disabled if the gate is open
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import 'jest-dom/extend-expect'
+import renderer from 'react-test-renderer'
+import Controls from './Controls'
 
+describe('<Control />', () => {
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Controls />); 
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+    it('should render btn to toggle closed and locked states', ()=>{
+      const { getByText } = render(<Controls/>)
+      getByText(/close gate/i)
+      getByText(/lock gate/i)
+    });
+
+    it(' should disble lock/unlock btn when open', ()=>{
+      const { getByText } = render(<Controls closed={false} />)
+      const btn = getByText(/lock gate/i)
+      expect(btn).toBeDisabled();
+    });
+
+    it('should disable open/close btn when locked', ()=>{
+        const {getByText} = render(<Controls locked={true} />)
+        const btn = getByText(/close gate/i)
+        expect(btn).toBeDisabled()
+    });
+
+    it(' should toggleClosed change on btn click', ()=>{
+        const toggleClosed = jest.fn();
+        const { getByText } = render(<Controls toggleClosed={toggleClosed}/>) 
+        const btn = getByText(/close gate/i)
+        fireEvent.click(btn)
+        expect(toggleClosed).toBeCalledTimes(1)
+    });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,12 +1,21 @@
 // Test: shows the controls and display
-import React from 'react';
-import * as rtl from 'react-testing-library';
-import Dashboard from './Dashboard';
+import React from 'react'
+import { render } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+import Dashboard from './Dashboard'
 
-describe('Dashboard', () => {
-    it("should render display and controls", () => {
-        const wrapper = rtl.render(<Dashboard />);
-        expect(wrapper.getByTestId(/controls/i));
-        expect(wrapper.getByTestId(/display/i));        
-    });
-})
+describe('<Dashboard />', () => {
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Dashboard />); 
+
+        expect(tree.toJSON()).toMatchSnapshot();
+      }); 
+
+    it('should render controls & display',() => {
+        const { getByText } = render(<Dashboard />)
+        getByText(/unlocked/i)
+        getByText(/lock gate/i)
+        getByText(/open/i)
+        getByText(/close gate/i)
+    })
+});
