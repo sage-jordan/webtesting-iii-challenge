602204338e99d2e036f4325aa1cbd3b6f3bf3a12
sage-jordan <sagemjrodan@gmail.com>
2019-10-03 14:03:45 -0400

    Dashboard renders display and controls, passed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,5 +1,6 @@
 // Test:
-//- provide buttons to toggle the `closed` and `locked` states.
+// - provide buttons to toggle the `closed` and `locked` states.
 // - buttons' text changes to reflect the state the door will be in if clicked
 // - the closed toggle button is disabled if the gate is locked
 // - the locked toggle button is disabled if the gate is open
+
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test: shows the controls and display
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import Dashboard from './Dashboard';
+
+describe('Dashboard', () => {
+    it("should render display and controls", () => {
+        const wrapper = rtl.render(<Dashboard />);
+        expect(wrapper.getByTestId(/controls/i));
+        expect(wrapper.getByTestId(/display/i));        
+    });
+})
