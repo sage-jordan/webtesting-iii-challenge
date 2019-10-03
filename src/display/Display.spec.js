376986a0c5e2e8899a7d54290d389d69d0aa6ed5
sage-jordan <sagemjrodan@gmail.com>
2019-10-03 13:57:32 -0400

    moved test to correct file

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import * as rtl from 'react-testing-library';
+import React from 'react';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+
+
+describe('Display', () => {
+    it("should render display component", () => {
+        const wrapper = rtl.render(<Display />);
+        expect(wrapper.getByTestId(/display/i));
+    });
+})
